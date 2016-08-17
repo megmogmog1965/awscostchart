c6e7a7a376e84bfa9124a51a89085b43c9928618
Kawatsu Yusuke <meg.mog.mog1956@gmail.com>
2016-08-17 21:43:55 +0900

    fixed header.

--- src/ts/Header.tsx
+++ src/ts/Header.tsx
@@ -29,14 +29,26 @@ export class Header extends React.Component<Props, State> {
   render () {
     return (
       <div className='Header'>
-        <div className='dropdown'>
-          <div className='dropbtn'>
-            <i className='fa fa-list' aria-hidden='true'></i>
-            <span>Menu</span>
+        <div className='left-align'>
+          <div className='v-align-middle'>
+            <div className='dropdown'>
+              <div className='dropbtn'>
+                <i className='fa fa-list' aria-hidden='true'></i>
+                <span>Menu</span>
+              </div>
+              <div className='dropdown-content'>
+                <a href='#/estimated_charge'>Estimated Charge</a>
+                <a href='#/monthly'>Monthly Costs</a>
+              </div>
+            </div>
           </div>
-          <div className='dropdown-content'>
-            <a href='#/estimated_charge'>Estimated Charge</a>
-            <a href='#/monthly'>Monthly Costs</a>
+        </div>
+
+        <div className='right-align'>
+          <div className='v-align-middle'>
+            <a href='https://github.com/megmogmog1965/awscostchart' >
+              <i className='fa fa-github fa-lg' aria-hidden='true'></i>
+            </a>
           </div>
         </div>
       </div>
